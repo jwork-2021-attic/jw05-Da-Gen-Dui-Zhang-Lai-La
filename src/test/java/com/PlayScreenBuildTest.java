1dfb2e38a076261f5273a513267a8ba60568f276
zjr191220158 <1159695291@qq.com>
2021-12-25 23:38:42 +0800

    jwork06

---
+++ src/test/java/com/PlayScreenBuildTest.java
@@ -0,0 +1,12 @@
+package com;
+import org.junit.Test;
+import java.awt.Color;
+import static org.junit.Assert.assertTrue;
+import screen.PlayScreen;
+public class PlayScreenBuildTest {
+    @Test
+    public void test(){
+        PlayScreen example;
+        assert(true);
+    }
+}
